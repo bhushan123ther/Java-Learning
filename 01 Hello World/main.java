a9717e51204b0d59c556a1fa016f911ff6bc6d9c
Bhushan  Ther <87998214+bhushan123ther@users.noreply.github.com>
2023-04-15 00:47:57 +0530

    Add files via upload

---
+++ 01 Hello World/main.java
@@ -0,0 +1,7 @@
+package com.javacodedaily;
+    public class Main {
+         public static void main(String[] args) {
+         System.out.println("Hello world");
+         System.out.println("I am Bhushan contributing in this repo");
+    }
+ }
