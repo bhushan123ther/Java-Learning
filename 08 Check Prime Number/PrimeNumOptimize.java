2ededbe8abab4478adc0e4ddd8ae46a7f56be171
Bhushan  Ther <87998214+bhushan123ther@users.noreply.github.com>
2023-04-16 17:20:54 +0530

    Add files via upload

---
+++ 08 Check Prime Number/PrimeNumOptimize.java
@@ -0,0 +1,24 @@
+package com.javacodedaily.functions;
+
+public class PrimeNumOptimize {
+
+	public static boolean isPrime(int n) {
+		if (n == 2) {
+			return true;
+		}
+
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(isPrime(3));
+
+	}
+
+}
